705dd09b207a5d4a0c76f168b48734841f714f65
jihun90 <jihun90.kim@icloud.com>
2022-07-03 12:32:12 +0900

    mlogn으로 구현/메모리초과/메모리침범되는듯

--- jihun90.kim/220707_backjoon11437.cpp
+++ jihun90.kim/220707_backjoon11437.cpp
@@ -1,94 +1,129 @@
 #include<stdio.h>
 #include<vector>
 #include<queue>
+#include<cmath>
 
 using namespace std;
 
 struct node
 {
-    node* parents = nullptr;
-    vector<node*> nodes;
+    vector<node*> parents;
+//    vector<node*> nodes;
     int cur = 0;
     int level = 0;
 };
 
 int _nodeCount;
+int _parentsSize;
 
 int LCA(node *tree, int a, int b)
 {
+    int aLevel = tree[a].level;
+    int bLevel = tree[b].level;
+
     int aCur = tree[a].cur;
     int bCur = tree[b].cur;
 
-    while (aCur != bCur)
+    if(aLevel > bLevel)
     {
-        int aLevel = tree[aCur].level;
-        int bLevel = tree[bCur].level;
-
-        if(aLevel > bLevel)
-        {
-            aCur = tree[aCur].parents->cur;
-        }
-        else if(aLevel < bLevel)
-        {
-            bCur = tree[bCur].parents->cur;
-        }
-        else
-        {   
-            aCur = tree[aCur].parents->cur;
-            bCur = tree[bCur].parents->cur;
-        }
+        aCur = tree[a].parents[aLevel - bLevel -1]->cur;
+    }
+    else if(aLevel < bLevel)
+    {
+        bCur = tree[b].parents[bLevel - aLevel - 1]->cur;
     }
     
+    int index = 0;
+    int prevACur = 0;
+    int prevBCur = 0;
+    bool isRecalc = false;
+    while(tree[aCur].cur != tree[bCur].cur)
+    {
+        if (!isRecalc)
+        {
+            isRecalc = true;
+        }
+        prevACur = aCur;
+        prevBCur = bCur;
+        
+        if(tree[aCur].parents.size() > pow(2,index))
+        {
+            aCur =tree[aCur].parents[pow(2, index)]->cur;
+            bCur =tree[bCur].parents[pow(2, index)]->cur;
+        }
+    }
+
+    index = 0;
+    if (isRecalc)
+    {
+        aCur = prevACur;
+        bCur = prevBCur;
+    }
+        
+    while(tree[aCur].cur != tree[bCur].cur)
+    {
+        aCur =tree[aCur].parents[index]->cur;
+        bCur =tree[bCur].parents[index]->cur;
+    }
+
     return aCur;
+
 }
 
 void makeTree(node *tree, vector<pair<int, int>> *edgeList)
 {   
-    int lev = 1;
-    tree[1].parents = &tree[0];
-    tree[1].level = lev;
-    tree[1].cur = 1;
-    
     queue<int> index;
     index.push(1);
+    int front = index.front();
+
+
     while(!index.empty())
     {
-        int front = index.front();
-        for(vector<pair<int,int>>::iterator it = edgeList->begin(); it != edgeList->end(); it++)
+        front = index.front();
+        for(int i = 0; i<edgeList->size(); i++)
         {
-            pair<int, int> temp = *it;
-            
-            if(tree[front].cur == temp.first)
+            pair<int, int> temp = edgeList->at(i);
+            if (tree[front].cur == temp.first)
             {
-                node* parentsNode = tree[temp.second].parents;
-                if((parentsNode == nullptr))
+                node tempNode;
+                tempNode.parents.resize(_parentsSize+1);
+                tempNode.cur = temp.second;
+                tempNode.level = tree[front].level + 1;
+                tempNode.parents[0] = &tree[front];
+                for(int j=1; j < tree[front].parents.size(); j++)
                 {
-                    node tempNode;
-                    tempNode.cur = temp.second;
-                    tempNode.parents = &tree[front];
-                    tempNode.level = lev+1;
-                    tree[front].nodes.push_back(&tempNode);
-                    tree[temp.second] = tempNode;
-                    index.push(temp.second);
-                }       
-            }       
-            else if(tree[front].cur == temp.second)
-            {
-                node* parentsNode = tree[temp.first].parents;
-                if((parentsNode == nullptr))
-                {
-                    node tempNode;
-                    tempNode.cur = temp.first;
-                    tempNode.parents = &tree[front];
-                    tempNode.level = lev+1;
-                    tree[front].nodes.push_back(&tempNode);
-                    tree[temp.first] = tempNode;
-                    index.push(temp.first);
+                    tempNode.parents[j] = tree[front].parents[j-1];
                 }
+ //               tree[front].nodes.push_back(&tempNode);
+                tree[tempNode.cur] = tempNode;
+
+                (*edgeList)[i].first = -1; //visited
+                (*edgeList)[i].second = -1;
+
+                index.push(tempNode.cur);
             }
+            else if (tree[front].cur == temp.second)
+            {
+                node tempNode;
+                tempNode.parents.resize(_parentsSize+1);
+                tempNode.cur = temp.first;
+                tempNode.level = tree[front].level + 1;
+                tempNode.parents[0] = &tree[front];
+                for(int j=1; j < tree[front].parents.size(); j++)
+                {
+                    tempNode.parents[j] = tree[front].parents[j-1];
+                }
+//                tree[front].nodes.push_back(&tempNode);
+                tree[tempNode.cur] = tempNode;
+
+                (*edgeList)[i].first = -1;
+                (*edgeList)[i].second = -1;
+
+                index.push(tempNode.cur);
+            }
+
         }
         index.pop();
-        lev++;
     }
 }
 
@@ -98,7 +133,7 @@ int main()
     vector<pair<int, int>> solList;
     
     scanf("%d", &_nodeCount);
-    node tree[_nodeCount+1];
+    node* tree = new node[_nodeCount+1];
 
     for(int i = 0; i < _nodeCount-1; i++)
     {
@@ -115,7 +150,17 @@ int main()
         scanf("%d %d", &a, &b);
         solList.emplace_back(a, b);
     }
+    
+    _parentsSize = (int)log2(_nodeCount) + 1;
 
+    for(int i = 0; i < _nodeCount+1; i++)
+    {
+        tree[i].parents.resize(_parentsSize);
+    }
+
+    int lev = 1;
+    tree[1].level = lev;
+    tree[1].cur = 1;
     makeTree(tree, &edgeList);
 
     for(int i = 0; i<solList.size(); i++)
@@ -123,5 +168,7 @@ int main()
         int result = LCA(tree, solList[i].first, solList[i].second);
         printf("%d\n", result);
     }
+    delete[] tree;
+
     return 0;
 }
