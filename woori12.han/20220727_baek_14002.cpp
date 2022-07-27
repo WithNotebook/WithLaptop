02fefc0b2cc4ff49594a18aaa655c30ff535fc88
mynameisdfg <woorih12@gmail.com>
2022-07-28 01:50:03 +0900

    [20220727] 14002 commit

---
+++ woori12.han/20220727_baek_14002.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int ary[1001] = { 0, };
+int lis[1001] = { 0, };
+vector<int> v;
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> ary[i];
+	}
+
+    int maxLength = 0;
+    int maxLengthIndex = 0;
+	for (int i = 0; i < N; i++)
+	{
+		lis[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (ary[j] < ary[i])
+			{
+                lis[i] = max(lis[i], lis[j] + 1);
+			}
+		}
+        if (maxLength < lis[i]) {
+            maxLength = lis[i];
+            maxLengthIndex = i;
+        }
+	}
+
+    for (int i = maxLengthIndex; i >= 1; i--) 
+    {
+        if (maxLength == lis[i]) 
+        {
+            v.push_back(ary[i]);
+            maxLength--;
+        }
+    }
+
+    int size = v.size();
+    printf("%d\n", size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", v.back());
+        v.pop_back();
+    }
+
+	return 0;
+}
