c2c9a9073d7494fef025408c4f14ece4a26dfbae
mynameisdfg <woorih12@gmail.com>
2022-08-11 01:51:16 +0900

    2610 commit(fail)

---
+++ woori12.han/20220810_baek_2610.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, M;
+int parent[101];
+
+int find(int n)
+{
+	if (parent[n] == n)
+	{
+		return n;
+	}
+
+	return parent[n] = find(parent[n]);
+}
+
+void merge(int a, int b)
+{
+	int parentA = find(a);
+	int parentB = find(b);
+
+	//if (parentA == parentB) return;
+
+	if (a < b)
+	{
+		if (parentA != a) //부모가 있다면
+		{
+			parent[parentA] = a;
+			parent[a] = a;
+			parent[b] = a;
+		}
+		else
+		{
+			parent[b] = a;
+		}
+	}
+	else
+	{
+		if (parentB != b) //부모가 있다면
+		{
+			parent[parentB] = b;
+			parent[a] = b;
+			parent[b] = b;
+		}
+		else
+		{
+			parent[a] = b;
+		}
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i <= N; i++)
+	{
+		parent[i] = i;
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		merge(a, b);
+	}
+
+	return 0;
+}
