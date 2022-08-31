9734a42e287f029074c25309093c38e47319eec1
ddaisyujin <35206992+ddaisyujin@users.noreply.github.com>
2022-09-01 02:10:08 +0900

    [라인스위핑] 13334
    라인스위핑 설명 : https://blogshine.tistory.com/120
    
    !!! 라인스위핑 반복문 하나로 풀려고 노력하기 !!!
    1. 투포인터, 우선순위큐
    2. end 지점으로 sort해서 List 만듦
    2.1. 최대값 찾기 배열의 담는 기준은 end!! 빼는 기준은 start!
    3. 배열의 순서가 중요
    4. 이중반복문 시간초과남

--- yujin21.shin/220901_b13334.cpp
+++ yujin21.shin/220901_b13334.cpp
@@ -40,16 +40,20 @@ int main() {
 	long long cnt=0;
 	priority_queue<int,vector<int>,greater<int>> pq;
 	for (int i = 0; i < peopleNum; i++) {
+		// 철로 길이보다 큰건 PASS
 		if (infoList[i].end - infoList[i].start > L) continue;
-
+		
+		// 현재 start 지점 갱신
 		if (infoList[i].start > start) {
 			start = infoList[i].end - L;
 			end = infoList[i].end;
 		}
-
+		
+		// 철로 길이 안에 들어오는 건  PUSH
 		if (infoList[i].end <= end)
 			pq.push(infoList[i].start);
-
+		
+		// start 지점을 갱신했으므로 배열도 갱신
 		while(pq.top() < start){
 				pq.pop();
 		}
