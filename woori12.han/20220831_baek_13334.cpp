539e4a420ed13db2acd1dea0e71ec579d9532d12
mynameisdfg <woorih12@gmail.com>
2022-09-01 08:15:17 +0900

    13334 commit

---
+++ woori12.han/20220831_baek_13334.cpp
@@ -0,0 +1,75 @@
+﻿#include<iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+bool comp(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	else
+	{
+		a.second < b.second;
+	}
+}
+
+vector<pair<int, int>> v;
+priority_queue<int> pq;
+
+int main()
+{
+	int s, e, length;
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> s >> e;
+
+		if (s <= e)
+		{
+			v.push_back({ s, e });
+		}
+		else
+		{
+			v.push_back({ e, s });
+		}
+	}
+
+	cin >> length;
+
+	sort(v.begin(), v.end(), comp);
+
+	int res = 0;
+	for (int i = 0; i < N; i++)
+	{
+		int start = v[i].first;
+		int end = v[i].second;
+
+		if (end - start > length)
+		{
+			continue;
+		}
+
+		pq.push(-start);
+
+		while (!pq.empty())
+		{
+			int num = -pq.top();
+			if (num + length >= end)
+			{
+				break;
+			}
+			pq.pop();
+		}
+		res = max(res, (int)pq.size());
+	}
+
+	cout << res << "\n";
+
+	return 0;
+}
