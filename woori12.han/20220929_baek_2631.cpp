8ffc81083510aa0e1b07f84adce10cb7f530e6af
mynameisdfg <woorih12@gmail.com>
2022-09-29 08:46:37 +0900

    2631 commit

---
+++ woori12.han/20220929_baek_2631.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int ary[202] = { 0, };
+int dp[202] = { 0, };
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> ary[i];
+		dp[i] = 1;
+	}
+
+	int maxDPNum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			if ((ary[i] > ary[j]) && ((dp[j] + 1) > dp[i]))
+			{
+				dp[i] = dp[j] + 1;
+			}
+		}
+		maxDPNum = max(maxDPNum, dp[i]);
+	}
+
+	cout << N - maxDPNum << endl;
+	return 0;
+}
