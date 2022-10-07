fde6717ea5b0d1edce1d706fb8867ceeb546cc5d
jihun90 <jihun90.kim@icloud.com>
2022-10-07 23:10:04 +0900

    baekjoon1725

--- jihun90.kim/221007_baekjoon1725.cpp
+++ jihun90.kim/221007_baekjoon1725.cpp
@@ -24,18 +24,17 @@ int main()
     lls.push(0);
     for(int i = 1; i < N+2; i++)
     {
-        if(!lls.empty())
+        while( !lls.empty() && (h[i] < h[lls.top()]))
         {
-            while(h[i] < h[lls.top()])
-            {
-                maxVal = max(maxVal, h[lls.top()] * (i - lls.top()));
-                lls.pop();
-            }
-
-            lls.push(i);
+            long long temp = lls.top();
+            lls.pop();
+            maxVal = max(maxVal, h[temp] * (i - lls.top() - 1));
         }
+
+        lls.push(i);
+    
     }
 
-    printf("%lld", maxVal);
+    printf("%lld\n", maxVal);
     return 0;
 }
