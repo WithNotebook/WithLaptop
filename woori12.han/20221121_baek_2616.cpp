80da7c105595cea729a1c8816933cd52a4997c84
mynameisdfg <woorih12@gmail.com>
2022-11-21 20:42:54 +0900

    2616 commit

---
+++ woori12.han/20221121_baek_2616.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<algorithm>
+
+using namespace std;
+int input[50001] = { 0, };
+int dp[4][50001] = { 0, };
+int main()
+{
+
+	int N;
+	cin >> N;
+
+	int a;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> a;
+		input[i] = input[i - 1] + a;
+	}
+
+	int numOfCars;
+	cin >> numOfCars;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = i * numOfCars; j <= N; j++)
+		{
+			dp[i][j] = max(dp[i][j - 1], dp[i - 1][j - numOfCars] + input[j] - input[j - numOfCars]);
+		}
+	}
+
+	cout << dp[3][N] << "\n";
+	return 0;
+}
