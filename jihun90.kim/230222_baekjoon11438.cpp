41473515022c7cc1c0527601576abf418246c09e
jihun90 <jihun90.kim@icloud.com>
2023-02-23 22:42:43 +0900

    [LCA]baekjoon11438 정답

--- jihun90.kim/230222_baekjoon11438.cpp
+++ jihun90.kim/230222_baekjoon11438.cpp
@@ -5,33 +5,26 @@
 using namespace std;
 
 #define N_MAX 100001
+#define LOG_N 20 //(log2(N_MAX))
 
-vector<int> graph[N_MAX];
-vector<int> parents[N_MAX];
-int nodeDepth[N_MAX];
-bool visited[N_MAX];
+vector<int> tree[N_MAX];
+int parents[N_MAX][LOG_N];
+int depth[N_MAX];
 
-int log_N;
-int N;
 
 int LCA(int a, int b)
 {
     int targetNode = a;
     int compareNode = b;
     
-    if(nodeDepth[targetNode] != nodeDepth[compareNode])
+    if(depth[targetNode] != depth[compareNode])
     {
-        if(nodeDepth[targetNode] < nodeDepth[compareNode]) swap(targetNode, compareNode);
+        if(depth[targetNode] < depth[compareNode]) swap(targetNode, compareNode);
         
-        for(int i=log_N-1; i>=0; i--)
+        for(int i=LOG_N-1; i>=0; i--)
         {
-            int targetDepth = nodeDepth[parents[targetNode][i]];
-            if (targetDepth == nodeDepth[compareNode]) 
-            {
-                targetNode = parents[targetNode][i]; 
-                break;
-            }
-            if(targetDepth >= nodeDepth[compareNode])
+            int targetDepth = depth[parents[targetNode][i]];
+            if(targetDepth >= depth[compareNode])
             {
                 targetNode = parents[targetNode][i];
             }
@@ -41,53 +34,42 @@ int LCA(int a, int b)
     int result = targetNode;
     if (targetNode != compareNode)
     {
-        for(int i=log_N-1; i>=0; i--) 
+        for(int i=LOG_N-1; i>=0; i--) 
         {
-            if(parents[targetNode][i] != 0)
+            if(parents[compareNode][i] != parents[targetNode][i])
             {
-                if(parents[compareNode][i] != parents[targetNode][i])
-                {
-                    targetNode = parents[targetNode][i];
-                    compareNode = parents[compareNode][i];
-                }
+                targetNode = parents[targetNode][i];
+                compareNode = parents[compareNode][i];
             }
-            
             result = parents[targetNode][0];//result = parents[targetNode][i]; ***
         }
     }
     return result;
 }
 
-void setParents() //*** 별표 3개!
+void makeTree(int cur)
 {
-    for(int j=1; j<log_N; j++)
+    for(int i=1; i<LOG_N; i++)
     {
-        for(int i=1; i<=N; i++)    
-        {
-            parents[i][j] = parents[parents[i][j-1]][j-1];
-        }
+        parents[cur][i] = parents[parents[cur][i-1]][i-1];
     }
-}
 
-void setTree(int cur)
-{
-    for(vector<int>::iterator it = graph[cur].begin(); it!=graph[cur].end(); it++)
+    for(auto it=tree[cur].begin(); it!=tree[cur].end(); it++)
     {
+        if (parents[cur][0] == (*it)) tree[cur].erase(it);
+        if(it == tree[cur].end()) break;
+
         int next = (*it);
-        if(!visited[next])
-        {
-            parents[next][0] = cur;
-            visited[next] = true;
-            nodeDepth[next] = nodeDepth[cur] +1;
-            setTree(next);
-        }
+        parents[next][0] = cur;
+        depth[next] = depth[cur] + 1;
+        makeTree(next);
     }
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-    
+    int N;
     cin >> N;
 
     for(int i=1; i<N; i++)
@@ -95,24 +77,18 @@ int main()
         int a, b;
         cin >> a >> b;
 
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        tree[a].push_back(b);
+        tree[b].push_back(a);
     }
-
-    log_N = log(N)+1;
 
     for(int i=0; i<=N; i++)
     {
-        nodeDepth[i] = 0;
-        parents[i].resize(log_N);
+        depth[i] = -1;
     }
 
-    visited[0] = true;
-    visited[1] = true;
-    nodeDepth[1] = 1;
     parents[1][0] = 0;
-    setTree(1);
-    setParents();
+    depth[1] = 0;
+    makeTree(1);
     
     int M=0; 
     cin >> M;
